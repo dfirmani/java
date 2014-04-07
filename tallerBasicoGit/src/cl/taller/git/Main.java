518c782f20da2d8332747240216cb9285da7f3c7
dfirmani <debiefirmani@yahoo.com>
2014-04-07 16:46:37 -0300

    tercer commit
    mi tercer commit

--- tallerBasicoGit/src/cl/taller/git/Main.java
+++ tallerBasicoGit/src/cl/taller/git/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("mi segundo commit");
+		System.out.println("una nueva linea desde la red");
 		
 	}
 
