dcf193667f4addc1528a47c8cba0376e3391a0a7
Daniel <Daniel@10.1.6.118>
2014-04-07 17:21:08 -0300

    commit 5

--- tallerBasicoGit/src/cl/taller/git/Main.java
+++ tallerBasicoGit/src/cl/taller/git/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("mi segundo commit");
-		System.out.println("una nueva linea desde la red");
+		System.out.println("una nueva linea desde la red. Prueba 2");
+		System.out.println("linea local");
 		
 	}
 
